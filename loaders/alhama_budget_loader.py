d82ca5c52b8ef9fd35e1375bcf48890345d8606b
Eduardo Sebastian <eduardo@civio.es>
2016-11-21 20:04:53 +0100

    Update the budget loader script
    Update the budget loader script in order to deal with the updated
    budget data files format.

--- loaders/alhama_budget_loader.py
+++ loaders/alhama_budget_loader.py
@@ -38,8 +38,8 @@ class AlhamaBudgetLoader(SimpleBudgetLoader):
         is_actual = (filename.find('/ejecucion_')!=-1)
         if is_expense:
             # We got 3- or 4- digit functional codes as input, so add a trailing zero
-            fc_code = line[0].ljust(4, '0')
-            ec_code = line[1]
+            fc_code = line[1].ljust(4, '0')
+            ec_code = line[2]
 
             # For years before 2015 we check whether we need to amend the programme code
             year = re.search('municipio/(\d+)/', filename).group(1)
@@ -56,16 +56,16 @@ class AlhamaBudgetLoader(SimpleBudgetLoader):
                 'ic_code': '000',
                 'item_number': ec_code[-2:],    # Last two digits
                 'description': line[3],
-                'amount': self._parse_amount(line[5 if is_actual else 3])
+                'amount': self._parse_amount(line[6 if is_actual else 4])
             }
 
         else:
             return {
                 'is_expense': False,
                 'is_actual': is_actual,
-                'ec_code': line[0][:-2],        # First three digits
+                'ec_code': line[1][:-2],        # First three digits
                 'ic_code': '000',               # All income goes to the root node
-                'item_number': line[0][-2:],    # Fourth and fifth digit; careful, there's trailing dirt
-                'description': line[1],
-                'amount': self._parse_amount(line[5 if is_actual else 2])
+                'item_number': line[1][-2:],    # Fourth and fifth digit; careful, there's trailing dirt
+                'description': line[2],
+                'amount': self._parse_amount(line[6 if is_actual else 3])
             }
