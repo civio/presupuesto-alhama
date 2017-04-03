7fec0279630d23fde92ed459a3990e0f96298c18
Eduardo Sebastian <eduardo@civio.es>
2017-04-03 21:43:45 +0200

    Update the payments loader script
    Mark unidentified payees as anonymized, clean up payee names and
    tokenize the rest of the fields.

--- loaders/alhama_payments_loader.py
+++ loaders/alhama_payments_loader.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
-import datetime
+import dateutil.parser
+import re
 
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
@@ -13,18 +14,57 @@ class AlhamaPaymentsLoader(PaymentsLoader):
         # But what we want as area is the policy description
         policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
 
-        payee = self._titlecase(line[12].strip())
+        date = line[0].strip()
+        date = dateutil.parser.parse(date, dayfirst=True).strftime("%Y-%m-%d")
+
+        payee = line[12].strip()
         payee = ("Otros" if payee == "" else payee)
+        payee = payee.replace(', ', ' ').replace(',', ' ')
+        payee = re.sub('\s\s+', ' ', payee)
+        payee = re.sub(r'CB$', 'C.B.', payee)
+        payee = re.sub(r'SL$', 'S.L.', payee)
+        payee = re.sub(r'S.L$', 'S.L.', payee)
+        payee = re.sub(r'S.L$', 'S.L.', payee)
+        payee = re.sub(r'S.L ', 'S.L. ', payee)
+        payee = re.sub(r'SLL$', 'S.L.L.', payee)
+        payee = re.sub(r'SLU$', 'S.L.U.', payee)
+        payee = re.sub(r'SA$', 'S.A.', payee)
+        payee = re.sub(r'SAU$', 'S.A.U.', payee)
+        payee = re.sub(r'SOC\. COOPERATIVA$', 'SDAD. COOP.', payee)
+        payee = re.sub(r'k-1$', 'K-1', payee)
+        payee = self._titlecase(payee)
+        payee = re.sub(r' E ', ' e ', payee)
+        payee = re.sub(r' I ', ' i ', payee)
+        payee = re.sub(r' Y ', ' y ', payee)
+        payee = re.sub(r' D\'', ' d\'', payee)
+        payee = re.sub(r' De ', ' de ', payee)
+        payee = re.sub(r' Del ', ' del ', payee)
+        payee = re.sub(r' La ', ' la ', payee)
+        payee = re.sub(r' Lo ', ' lo ', payee)
+        payee = re.sub(r'Ii$', 'II', payee)
+        payee = re.sub(r'^Amg ', 'AMG ', payee)
+        payee = re.sub(r'^Adinor ', 'ADINOR ', payee)
+        payee = re.sub(r'Itc ', 'ITC ', payee)
+
+        anonymized = False
+        anonymized = (True if payee == "Otros" else anonymized)
+        anonymized = (True if payee == "Usuario Servicios Sociales" else anonymized)
+
+        description = line[14].strip()[:300]
+        description = description.decode('utf-8','ignore').encode('utf-8')
+
+        amount = line[2].strip()
+        amount = self._read_english_number(amount)
 
         return {
             'area': policy,
             'programme': None,
             'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
             'ec_code': None,
-            'date': line[0].strip(),
+            'date': date,
             'contract_type': None,
             'payee': payee,
-            'anonymized': False,
-            'description': line[14].strip(),
-            'amount': self._read_english_number(line[2])
+            'anonymized': anonymized,
+            'description': description,
+            'amount': amount
         }
