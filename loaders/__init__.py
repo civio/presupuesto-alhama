b72009da9baaf7bed5ee581917616eab092a2dff
David Cabo <david.cabo@gmail.com>
2025-03-20 22:20:08 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,2 +1,8 @@
-from alhama_budget_loader import AlhamaBudgetLoader
-from alhama_payments_loader import AlhamaPaymentsLoader
+import six
+
+if six.PY2:
+    from alhama_budget_loader import AlhamaBudgetLoader
+    from alhama_payments_loader import AlhamaPaymentsLoader
+else:
+    from .alhama_budget_loader import AlhamaBudgetLoader
+    from .alhama_payments_loader import AlhamaPaymentsLoader
